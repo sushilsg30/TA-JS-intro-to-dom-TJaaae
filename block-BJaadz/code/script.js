e371f5fc0d5798cd0607148b99154fe041aa6c0c
Sushil Ghate <sushilsg30@gmail.com>
2022-04-26 23:09:54 +0530

    block-BJaadz

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,37 +1,69 @@
 // Select the h1 element and store it in a variable named heading.
 
+let heading = document.querySelector('h1');
+
 // Check the typeof heading and log it.
+
+console.log(typeof heading);
 
 // Change the color of heading to black.
 
+heading.style.color = "black";
+
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
+
+let allHrs = document.querySelectorAll('hr');
+
 
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
 Array.from converts an array kind of data into array so we can use methods like map, reduce
-
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
+let allHrsArray = Array.from(allHrs);
 
 // Set the border of the all the hr elements to "1px solid tomato"
 
+allHrsArray.map((hr) => hr.style.border = "1px solid tomato");
+
+ 
+
 // Change the background of all the hr to "antiquewhite" using for of loop.
+
+for(let hr of allHrsArray) {
+
+  hr.style.background = "antiquewhite";
+
+}
 
 // Change the 'border-radius' of all the hr to "5px" using array.
 
+allHrsArray.map((hr) => hr.style.borderRadius = '5px');
+
 // Change the alignment of the heading(h1) to center.
+
+heading.style.textAlign = 'center';
 
 // Change the font size of the heading to 3rem.
 
+heading.style.fontSize = '3rem';
+
 // Change the border of hr with class 'image' to `2px solid purple`.
+
+document.querySelector('.image').style.border = `2px solid purple`;
 
 // Hide the box number 17 (last box).
 
+// document.querySelector('.box seventeen').style.border = "red";
+
 // Change the border of all the hr element from solid to dashed type
 
+allHrsArray.map((hr) => hr.style.border = "1px dashed red");
+
 // Create a pragraph element and store it in variable named 'para' using `createElement`
+
 
 // Change the inner text of para to "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
 
